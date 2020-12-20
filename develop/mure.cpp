1092db49ab1c5fe9bdb651149b5a5a5fcaf74623
MedAnisse <Med-anisse.belhadj@outlook.com>
2020-12-20 20:37:12 +0100

    changement de la classe Mure

--- develop/mure.cpp
+++ develop/mure.cpp
@@ -1,2 +1,39 @@
 #include "mure.h"
+namespace cassebrique
+{
+mure::mure(point& BasGauche,point& HautDroit,unique_ptr<surface>surfaceK):d_HautDroit{HautDroit},d_BasGauche{BasGauche}
+{
+    d_surface=move(surfaceK);
+}
+mure::mure(point& BasGauche,double hauteur,double largeur):d_BasGauche{BasGauche}
+{
+    d_HautDroit{d_BasGauche.x()+largeur,d_BasGauche.y()-hauteur};
+    d_surface=move(surfaceK);
+}
+mure::mure(double BasGauche_x,double BasGauche_y,double HautDroit_x,double HautDroit_y):
+    d_HautDroit{{BasGauche_x,BasGauche_y}},d_BasGauche{{HautDroit_x,HautDroit_y}}
+    {
+    d_surface=move(surfaceK);
+    }
 
+ mure::~mure()
+{
+
+}
+mure::mure()
+{
+
+}
+surface* mure::surfaceType()const
+{
+    return d_surface.get();
+}
+double mure::Hauteur()const
+{
+    return d_HautDroit.y()-d_BasGauche.y();
+}
+double mure::Largeur()const
+{
+    d_HautDroit.x()-d_BasGauche.x();
+}
+}
--- develop/mure.h
+++ develop/mure.h
@@ -5,10 +5,14 @@ namespace cassebrique
 class mure
 {
 public:
-    mure(point& BasGauche,point& HautDroit);
-    mure(point& BasGauche,double hauteur,double largeur);
-    mure(double BasGauche_x,double BasGauche_y,double HautDroit_x,double HautDroit_y);
-    mure(double BasGauche_x,double BasGauche_y,double BasGauche_x,double BasGauche_y);
+    mure(point& BasGauche,point& HautDroit,unique_ptr<surface>surfaceK);
+    mure(point& BasGauche,double hauteur,double largeur,unique_ptr<surface>surfaceK);
+    mure(double BasGauche_x,double BasGauche_y,double HautDroit_x,double HautDroit_y,unique_ptr<surface>surfaceK);
+    virtual ~mure();
+    mure();
+    surface* surfaceType()const;
+    double Hauteur()const;
+    double Largeur()const;
 
 private:
     unique_ptr<surface> d_surface;

d472fff96915fe7fa605edf04cd26acf87ab371a
MedAnisse <Med-anisse.belhadj@outlook.com>
2020-12-20 20:36:20 +0100

    changement de la classe Raquette

--- develop/Raquette.cpp
+++ develop/Raquette.cpp
@@ -1,2 +1,33 @@
 #include "Raquette.h"
+namespace cassebrique
+{
+     Raquette::Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK)
+     d_position{position},d_hauteur{hauteur},d_largeur{largeur}
+     {
+         d_surface=move(surfaceK);
+     }
+    Raquette::~Raquette()
+     {
 
+     }
+    surface* Raquette::surfaceType()const
+     {
+        return d_surface.get();
+     }
+    double Raquette::Hauteur()const
+     {
+         return d_hauteur;
+     }
+    double Raquette::Largeur()const
+     {
+        return d_largeur;
+     }
+     void bougerGauche(int n)
+     {
+    // changer la position de raquette vers la gauche avec n pas
+     }
+    void bougerDroit(int n)
+    {
+    // changer la position de raquette vers la droit avec n pas
+    }
+}
--- develop/Raquette.h
+++ develop/Raquette.h
@@ -5,11 +5,17 @@ namespace cassebrique
 class Raquette
 {
 public:
-    Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surface);
+    Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK);
     virtual ~Raquette();
+    surface* surfaceType()const;
+    double Hauteur()const;
+    double Largeur()const;
+    void bougerGauche(int n);
+    void bougerDroit(int n);
 private:
     unique_ptr<surface> d_surface;
-    point position
+    point position;
+    double d_hauteur,d_largeur;
 protected:
 
 };

d934c0a54aa840aa2643a39aa23e88046885a204
MedAnisse <Med-anisse.belhadj@outlook.com>
2020-12-21 19:22:15 +0100

    changement de la classe Raquette

--- develop/Raquette.h
+++ develop/Raquette.h
@@ -1,23 +1,28 @@
 #ifndef RAQUETTE_H
 #define RAQUETTE_H
+#include "rectangle.h"
+#include "surface.h"
+#include"surfaceNormal.h"
+#include<memory>
+using geom::point;
+using std::unique_ptr;
+using std::make_unique;
 namespace cassebrique
 {
-class Raquette
+class Raquette: public rectangle
 {
 public:
-    Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK);
+    Raquette();
+    Raquette(const point& BasGauche,const point& HautDroit,unique_ptr<surface>  surfaceK=make_unique<surfaceNormal>());
+    Raquette(point& BasGauche,double hauteur,double largeur,unique_ptr<surface>surfaceK=make_unique<surfaceNormal>());
     virtual ~Raquette();
     surface* surfaceType()const;
-    double Hauteur()const;
-    double Largeur()const;
-    void bougerGauche(int n);
-    void bougerDroit(int n);
+    void move_Gauche();
+    void move_Droit();
+    void move_Gauche(int n);
+    void move_Droit(int n);
 private:
     unique_ptr<surface> d_surface;
-    point position;
-    double d_hauteur,d_largeur;
-protected:
-
 };
 }
 #endif // RAQUETTE_H

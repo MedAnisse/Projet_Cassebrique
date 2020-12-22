189b43e18116b65e37e4caa3a6198484827170a8
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2020-12-22 22:15:38 +0100

    Add files via upload

---
+++ develop/ball.h
@@ -0,0 +1,26 @@
+#ifndef BALL_H
+#define BALL_H
+#include "point.h"
+
+namespace cassebrique
+{
+
+
+class ball {
+
+     public :
+
+         ball(point &position,double vitesse, double rayon);
+         ball(double x,double y, double rayon);
+         double rayon() const;
+         double vitesse() const;
+         point position() const;
+         balle& position(double Dt);
+         void affiche(std::ostream& ost) const;
+
+     private :
+        double d_vitesse,d_rayon;
+        point d_position;
+};
+}
+#endif // BALL_H

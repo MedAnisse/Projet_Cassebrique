4277e1640b94449d9cd790ea6a5adf15a176d2ec
MedAnisse <Med-anisse.belhadj@outlook.com>
2020-12-23 17:23:29 +0100

    modifiction de Raquette

--- develop/Raquette.cpp
+++ develop/Raquette.cpp
@@ -3,58 +3,38 @@
 #include<iostream>
 #include<ostream>
 #include<istream>
-#include"point.h"
-#include"surface.h"
-#include"surfaceNormal.h"
-#include "rectangle.h"
 using std::unique_ptr;
+using std::move;
 using std::make_unique;
-using geom::point;
-class balle;
 namespace cassebrique
 {
-    Raquette::Raquette():rectangle{}
-    {
-    d_surface=make_unique<surfaceNormal>();
-    }
-    Raquette::Raquette(const point& BasGauche,const point& HautDroit,unique_ptr<surface>surfaceK):rectangle{BasGauche,HautDroit}
-{
-    d_surface=move(surfaceK);
-}
-    Raquette::Raquette(point& BasGauche,double hauteur,double largeur,unique_ptr<surface>surfaceK):rectangle{BasGauche,hauteur,largeur}
-{
-    d_surface=move(surfaceK);
-}
+     Raquette::Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK)
+     d_position{position},d_hauteur{hauteur},d_largeur{largeur}
+     {
+         d_surface=move(surfaceK);
+     }
     Raquette::~Raquette()
-    {
+     {
 
-    }
+     }
     surface* Raquette::surfaceType()const
-    {
+     {
         return d_surface.get();
-    }
-    void Raquette::move_Gauche()
+     }
+    double Raquette::Hauteur()const
+     {
+         return d_hauteur;
+     }
+    double Raquette::Largeur()const
+     {
+        return d_largeur;
+     }
+     void bougerGauche(int n)
+     {
+    // changer la position de raquette vers la gauche avec n pas
+     }
+    void bougerDroit(int n)
     {
-        rectangle::d_BasGauche.move(0.0,-1.0);
-        rectangle::d_HautDroit.move(0.0,-1.0);
-    }
-    void Raquette::move_Droit()
-    {
-        rectangle::d_BasGauche.move(0.0,1.0);
-        rectangle::d_HautDroit.move(0.0,1.0);
-    }
-    void Raquette::move_Gauche(int n)
-    {
-        for(int i=0 ;i<n;i++)
-        {
-            move_Gauche();
-        }
-    }
-    void Raquette::move_Droit(int n)
-    {
-        for(int i=0 ;i<n;i++)
-        {
-            move_Droit();
-        }
+    // changer la position de raquette vers la droit avec n pas
     }
 }
--- develop/Raquette.h
+++ develop/Raquette.h
@@ -1,28 +1,23 @@
 #ifndef RAQUETTE_H
 #define RAQUETTE_H
-#include "rectangle.h"
-#include "surface.h"
-#include"surfaceNormal.h"
-#include<memory>
-using geom::point;
-using std::unique_ptr;
-using std::make_unique;
 namespace cassebrique
 {
-class Raquette: public rectangle
+class Raquette
 {
 public:
-    Raquette();
-    Raquette(const point& BasGauche,const point& HautDroit,unique_ptr<surface>  surfaceK=make_unique<surfaceNormal>());
-    Raquette(point& BasGauche,double hauteur,double largeur,unique_ptr<surface>surfaceK=make_unique<surfaceNormal>());
+    Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK);
     virtual ~Raquette();
     surface* surfaceType()const;
-    void move_Gauche();
-    void move_Droit();
-    void move_Gauche(int n);
-    void move_Droit(int n);
+    double Hauteur()const;
+    double Largeur()const;
+    void bougerGauche(int n);
+    void bougerDroit(int n);
 private:
     unique_ptr<surface> d_surface;
+    point position;
+    double d_hauteur,d_largeur;
+protected:
+
 };
 }
 #endif // RAQUETTE_H

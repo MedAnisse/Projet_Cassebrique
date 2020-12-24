102d7466e002faa5e699131f76c7e0f8afd2c3c6
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2020-12-24 20:05:51 +0100

    Add files via upload

---
+++ develop/ball.h
@@ -0,0 +1,33 @@
+#ifndef BALL_H
+#define BALL_H
+#include "point.h"
+#include "vector.h"
+#include<iosfwd>
+
+
+namespace geom
+{
+class point;
+
+class ball {
+
+     public :
+
+         ball(point &position,vector &direction,double vitesse, double rayon);
+         ball(double x,double y,double xd,double yd, double rayon);
+         double rayon() const;
+         double vitesse() const;
+         point position() const;
+         vector direction() const;
+         void setVitesse(double vitesse);
+         ball* position(double Dt);
+
+
+     private :
+        double d_vitesse,d_rayon;
+        point d_position;
+        vector d_direction;
+};
+
+}
+#endif // BALL_H

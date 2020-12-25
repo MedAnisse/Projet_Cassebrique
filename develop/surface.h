ca8c3c9fc557d83a1b22e46cbccbc14972bcdd41
asmaeya <58422384+asmaeya@users.noreply.github.com>
2020-12-25 16:35:31 +0100

    update surface

--- develop/surface.h
+++ develop/surface.h
@@ -1,14 +1,27 @@
 #ifndef SURFACE_H
 #define SURFACE_H
+#include "balle.h"
+#include "vector.h"
+#include <memory>
 
-namespace cassebrique
-{
 class surface
 {
     public:
-
+        surface();
+        surface(const geom::point& a, const geom::point& b);
         virtual ~surface();
-        virtual bool collusion(balle& balle) = 0;
+        geom::point a()const;
+        geom::point b()const;
+        virtual bool collision(balle& b);
+        std::unique_ptr<surface> copie();
+        double distance(balle& b);
+        //double distanceHorizontal(balle& b);
+        //double distanceVertical(balle& b);
+        void moveTo(geom::vector& v);
+        virtual void print(std::ostream& ost) const;
+        virtual void read(std::istream& ist);
+    private:
+        geom::point d_a,d_b;
 };
-}
+
 #endif // SURFACE_H
--- develop/surfaceTueuse.h
+++ develop/surfaceTueuse.h
@@ -1,11 +1,18 @@
 #ifndef SURFACETUEUSE_H
 #define SURFACETUEUSE_H
-namespace cassebrique
-{
-class surfaceTueuse
+#include <vector>
+#include "balle.h"
+#include "surface.h"
+
+class surfaceTueuse : public surface
 {
     public:
-      virtual bool collusion(balle& balle) override;
+        surfaceTueuse(const geom::point& a, const geom::point& b);
+        virtual ~surfaceTueuse();
+        virtual bool collision(balle& b) override;
+        std::unique_ptr<surfaceTueuse> copie();
+        virtual void print(std::ostream& ost) const override;
+        virtual void read(std::istream& ist) override;
 };
-}
+
 #endif // SURFACETUEUSE_H

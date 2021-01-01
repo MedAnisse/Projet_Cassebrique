95c663545b42a92aefd21d4fc40c7addff033860
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-01-01 17:50:31 +0100

    Update ball.cpp

--- develop/ball.cpp
+++ develop/ball.cpp
@@ -25,9 +25,13 @@ double ball :: vitesse() const
  {
      return d_direction;
  }
-ball* ball:: position(double Dt)
+ball* ball:: position(double dt)
  {
-       //
+          
+       double x= d_x+vitesse*cos(angle*3.14/180)*dt;
+       double y=d_y+0.5*pow(dt,2.0)+ vitesse*sin(angle*3.14/180)*dt;
+       d_position.moveTo(x, y);
+
  }
 
 

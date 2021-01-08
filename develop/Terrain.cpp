8d82188adb0517f17bb44cee4b6ea25f155577a4
MedAnisse <Med-anisse.belhadj@outlook.com>
2021-01-08 14:21:34 +0100

    copie Terrain de test_terrain ver develop

--- develop/Terrain.cpp
+++ develop/Terrain.cpp
@@ -1,95 +1,311 @@
 #include "Terrain.h"
+#include"graphics.h"
 #include<vector>
+#include"vector.h"
 #include<iostream>
 #include<memory>
-
+#include "surfaceDure.h"
+#include "surfaceMolle.h"
+#include "surface.h"
+#include "surfaceTueuse.h"
+#include "brique.h"
+#include"mur.h"
+#include "Raquette.h"
+#include"list.h"
+#include "balle.h"
+#include"briqueCassable.h"
+#include"briqueCassableApresPlusieurFois.h"
+#include"briqueIncassable.h"
+#include"afficheur.h"
+#include"balle.h"
 using std::vector;
+using std::make_unique;
+using std::cout;
+using std::endl;
 namespace cassebrique
 {
-unique_ptr<surface> Terrain::randomSurface()
+  Terrain::Terrain()
+    {
+      Terrain(700,900,5,5,10,20,40);
+    }
+    Terrain::Terrain(double hauteurTerrain, double largeurTerrain)
+    {
+      Terrain(hauteurTerrain,largeurTerrain,5,5,20 ,20,40);
+    }
+  Terrain::Terrain(double hauteurTerrain, double largeurTerrain,int nombreColone,int nombreLigne,double hauteurmur,double hauteurRaquette,double largeurRaquette):
+    d_largeur{largeurTerrain},
+    d_hauteur{hauteurTerrain},
+    d_murs{},
+    d_briques{},
+    d_raquette{}
+    {
+      initializer_Murs(hauteurmur);
+      initializer_Raquette(hauteurRaquette,largeurRaquette,hauteurmur);
+      initializer_Briques(nombreColone,nombreLigne,hauteurmur);
+      initializer_Balle();
+    }
+unique_ptr<surface>  Terrain::randomSurface()
 {
-int randNum = rand()%(3+ 1) ;
-switch(randNum)
-{
-   case 0:
-       return make_unique<surfaceDure>();
-    break;
-    case 1:
-       return make_unique<surfaceNormal>();
-    break;
-    case 2:
-      return  make_unique<surfaceMolle>();
-    break;
+  int randNum = rand()%(2+ 1) ;
+  switch(randNum)
+    {
+      case 0:
+        return make_unique<surfaceDure>();
+          break;
+      case 1:
+        return make_unique<surface>();
+          break;
+      case 2:
+        return  make_unique<surfaceMolle>();
+          break;
+    }
 }
+brique*  Terrain::randomBrique(point &position,double largeur,double hauteurBrique,int nombreFois)
+{
+  int randNum = rand()%(2+ 1) ;
+  switch(randNum)
+    {
+      case 0:
+        return new briqueCassable (position,hauteurBrique,largeur,randomSurface());
+          break;
+      case 1:
+        return new briqueIncassable (position,hauteurBrique,largeur,randomSurface());
+          break;
+      case 2:
+        return  new briqueCassableApresPlusieurFois (nombreFois,position,hauteurBrique,largeur,randomSurface());
+          break;
+    }
+    return nullptr;
 }
 void Terrain::initializer_Murs(double hauteur)
 {
-    d_mure.push_back(make_unique<mure>(0.0,-hauteur,d_largeur,0.0,make_unique<surfaceNormal>()));
-    d_mure.push_back(make_unique<mure>(0.0,-d_hauteur+hauteur,hauteur,-hauteur,make_unique<surfaceNormal>()));
-    d_mure.push_back(make_unique<mure>(d_largeur-hauteur,-d_hauteur+hauteur,d_largeur,-hauteur,make_unique<surfaceNormal>()));
-    d_mure.push_back(make_unique<mure>(0.0,-d_hauteur,d_largeur,-d_hauteur,make_unique<surfaceTueuse>()));
+    point a{0.0,hauteur};
+    point b{d_largeur,0.0};
+    point sa{hauteur,hauteur};
+    point sb{d_largeur-hauteur,hauteur};
+    unique_ptr<surface> t=make_unique<surface>(sa,sb);
+    d_murs.push_back(make_unique<cassebrique::mur>(a,b,t));
+    a=point(0.0,d_hauteur-hauteur);
+    b=point(hauteur,hauteur);
+    sa=point(hauteur,d_hauteur-hauteur);
+    t=make_unique<surface>(sa,b);
+    d_murs.push_back(make_unique<cassebrique::mur>(a,b,t));    a=point(d_largeur-hauteur,+d_hauteur-hauteur);
+    a=point(d_largeur-hauteur,d_hauteur-hauteur);
+    b=point(d_largeur,hauteur);
+    sb=point(d_largeur-hauteur,hauteur);
+     t=make_unique<surface>(a,sb);
+    d_murs.push_back(make_unique<cassebrique::mur>(a,b,t));
+    a=point(0.0,d_hauteur);
+    b=point(d_largeur,d_hauteur-hauteur );
+    sa=point(0.0,d_hauteur-hauteur );
+     t=make_unique<surfaceTueuse>(sa,b);
+    d_murs.push_back(make_unique<cassebrique::mur>(a,b,t));
 }
-void Terrain::initializer_Briques(int nbColone,int nbLigne,double hauteur)
+void Terrain::initializer_Briques(int nombreColone,int nombreLigne,double hauteurmur)
 {
-    //au permier lieu on fait une matrice de n*m brique n=10 , m=5
-    //le remier mure est le mure de haut , la hauteur de chaque mure est 10
-    //hauteur de mure
+    double largeurBrique=(d_largeur-(2*hauteurmur))/nombreColone ;
+    largeurBrique-=1.1;
+    double hauteurBrique=(d_hauteur-2*hauteurmur)/(2*nombreLigne) ;
 
-    double largeur=(d_largeur-2*hauteur)/n ;
-    double hauteur1=(d_hauteur-2*hauteur)/2/m ;
-    //hauteur1 c'est la hauteur de chaque brique
-    point position{hauteur+1,-hauteur-1};
-    for(int i=0,i<10,i++)
+    //cout<<"largeurbrique= "<<largeurBrique<<"  hauteurBrique  ="<<hauteurBrique;
+    point position{hauteurmur+1,hauteurmur+1};
+    double x{hauteurmur+1},y{hauteurmur+1};
+    for(int i=0;i<nombreLigne;i++)
     {
-            for(int j=0,j<5,j++)
-            {
-                surface surfaceRandom=move(randomSurface());
-                d_brique.push_back(make_unique<brique>(position,hauteur1,largeur,surfaceRandom));
+      for(int j=0;j<nombreColone;j++)
+        {
+            //cout<<" 1 position= "<<position<<endl;
+            d_briques.push_back(randomBrique(position,largeurBrique,hauteurBrique));
+            x+=largeurBrique+1;
+            y=position.y();
+            position.moveTo(x,y);
 
-                position.moveTo(position.x()+largeur+1,hauteur1);
-            }
-            position.moveTo(hauteur+1,position.y()-1);
+        }
+        x=hauteurmur+1;
+        y+=hauteurBrique+1;
+      position.moveTo(x,y);
+      //cout<<"2 position= "<<position<<endl;
     }
 }
-void Terrain::initializer_Raquette(point &position,double hauteur,double largeur)
+void Terrain::initializer_Raquette(double hauteurRaquette,double largeurRaquette,double hauteurmur)
 {
-//raquette(point , hauteur,largeur,surface)
-    d_raquette=make_unique<Raquette>(point(d_largeur/2,-d_hauteur),10,50,make_unique<surfaceNormal>());
+  double positionRaquetteX=d_largeur/2;
+  double positionRaquetteY=d_hauteur-hauteurRaquette-hauteurmur;
+  point positionRaquette{positionRaquetteX,positionRaquetteY};
+  d_raquette=make_unique<cassebrique::Raquette>(positionRaquette,hauteurRaquette,largeurRaquette);
 }
-void Terrain::initializer_Balle()
+void Terrain::initializer_Balle(double vitesse)
 {
-d_balle{};
+ double positionBalleY=d_hauteur/2;
+ double positionBalleX=d_largeur/2;
+ point positionBalle{positionBalleX,positionBalleY};
+ double rayonBalle=9;
+ double ongleBalle=0.25;
+    d_balle=make_unique<cassebrique::balle>(positionBalle,ongleBalle,vitesse,rayonBalle);
 }
-void Terrain::initializer_Balle(vectorposition & vect,double vitesse)
-{
- //balle(point(x,y),vecteurposition(dx,dy),vitesse)
-    d_balle{point(d_hauteur/2,d_largeur/2),vect,vitesse}
-    //point(d_hauteur/2,d_largeur/2) = centre de terain
-}
-Terrain::Terrain( double hauteur, double largeur)
-d_largeur{largeur},d_hauteur{hauteur},d_mure{} ,d_brique{} ,d_raquette{}, d_balle{}
-{}
 
-~Terrain::Terrain()
+double Terrain::hauteur() const
+{
+  return d_hauteur;
+}
+double Terrain::largeur()const
+{
+  return d_largeur;
+}
+Terrain::~Terrain()
 {}
-vector<mure*> Terrain::Tmure() const
+vector<cassebrique::mur*> Terrain::murs() const
 {
-    return d_mure[0].get();
-}
-vector<brique*> Terrain::Tbrique() const
-{
-    return d_brique[0].get();
-}
-Raquette* Terrain::TRaquette() const
-{
-    return d_raquette.get();
-}
-balle* Terrain::Tballe() const
-{
-    return d_balle.getI();
-}
-void Terrain::run(double dt)
-{
+  vector<cassebrique::mur*> tmpmurs;
+  for(auto& i : d_murs)
+    {
+      cassebrique::mur* tmpmur = dynamic_cast<cassebrique::mur*>(i.get());
+      if(tmpmur!=nullptr)
+        {
+          tmpmurs.push_back(tmpmur);
+        }
+    }
 
+  return tmpmurs;
+}
+list Terrain::briques()const
+{
+    return d_briques;
+}
+Raquette* Terrain::Raquette() const
+{
+  return d_raquette.get();
+}
+balle* Terrain::balle() const
+{
+  return d_balle.get();
+}
+
+Terrain* Terrain::terrain()
+{
+  return this;
+}
+vector<rectangle*> Terrain::rectangle() const
+{
+  std::vector<cassebrique::rectangle*> tmpRectangles= d_briques.tovector();
+  for(auto& i : d_murs)
+   {
+    tmpRectangles.push_back(i.get());
+   }
+  return tmpRectangles;
+}
+
+void Terrain::afficheRectangle(afficheur*  affiche)
+{
+    vector<cassebrique::rectangle*>tmpRectangle=rectangle();
+    for(auto& i : tmpRectangle)
+      {
+        affiche->afficherRectangle(i);
+      }
+}
+void Terrain::afficheRaquette(afficheur*  affiche)
+{
+  affiche->afficherRaquette(d_raquette.get());
+}
+void Terrain::afficheBalle(afficheur*  affiche)
+{
+  affiche->afficherBalle(balle());
+}
+void Terrain::RaquetteMoveGauche()
+{
+  d_raquette->move_Gauche();
+}
+void Terrain::RaquetteMoveDroit()
+{
+  d_raquette->move_Droit();
+}
+void Terrain::affichemurs(afficheur* affiche)
+{
+    vector<mur*> tmpmurs=murs();
+    for(auto& e:tmpmurs)
+    {
+        affiche->afficherRectangle(e);
+    }
+}
+void Terrain::run(double dt,afficheur* affiche)
+{
+  double temp=dt;
+  afficheRectangle(affiche);
+  afficheRaquette(affiche);
+  while(1)
+    {
+      d_balle->position(temp);
+      affiche->effacerContenu();
+      afficheBalle(affiche);
+      //afficheRectangle(affiche);
+      affichemurs(affiche);
+      afficheRaquette(affiche);
+      char i='g';
+      //afficheBalle(affiche);
+      i=affiche->caractereEntree();
+      if(i=='d' )
+        {
+          affiche->effacerContenu();
+          RaquetteMoveDroit();
+          //afficheRectangle(affiche);
+                affichemurs(affiche);
+          afficheRaquette(affiche);
+        }
+      if(i=='a' )
+        {
+          affiche->effacerContenu();
+          RaquetteMoveGauche();
+          //afficheRectangle(affiche);
+                affichemurs(affiche);
+          afficheRaquette(affiche);
+        }
+      temp+=dt;
+     }
+
+}
+bool Terrain::existColusion(cassebrique::balle* b)
+{
+  bool k=false;
+  for(int i=0,taillTab=d_murs.size() ;i<taillTab;i++)
+    {
+
+          k= k || d_murs[i]->OnCollision(b);
+          std::cout<<d_murs[i]->distance(b)<<std::endl;
+    }
+  vector<cassebrique::rectangle*> f= d_briques.tovector();
+    for(int i=0,len=f.size();i<i;i++)
+      {
+        cassebrique::brique* p= dynamic_cast<cassebrique::brique*>(f[i]);
+        if(p!=nullptr)
+        {
+          k=k || p->OnCollision(b);
+        }
+        cassebrique::briqueCassable* tmp2= dynamic_cast<cassebrique::briqueCassable*>(f[i]);
+        if(tmp2!=nullptr)
+          {
+            d_briques.erase(i);
+          }
+      }
+   /* list tmp =d_briques;
+    int i=0;
+    element* eleTmp=tmp.tete;
+    while(eleTmp!=nullptr)
+    {
+        cassebrique::brique* p= dynamic_cast<cassebrique::brique*>(eleTmp->info);
+    if(p!=nullptr)
+    {
+        k= p->OnCollision(b);
+    }
+     cassebrique::briqueCassable* tmp2= dynamic_cast<cassebrique::briqueCassable*>(eleTmp->info);
+        if(tmp2!=nullptr)
+        {
+            d_briques.erase(i);
+        }
+        ++i;
+        eleTmp=eleTmp->next;
+    }*/
+
+  k = k|| d_raquette->OnCollision(b);
+  return k;
 }
 }
--- develop/Terrain.h
+++ develop/Terrain.h
@@ -1,34 +1,61 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
+#include "rectangle.h"
+#include <memory>
+#include"mur.h"
+#include"Raquette.h"
+#include"brique.h"
+#include "balle.h"
+#include "surface.h"
+#include"list.h"
+#include"balle.h"
+#include"afficheurWinBgi.h"
 using std::vector;
+using std::unique_ptr;
+
+
 namespace cassebrique
 {
 class Terrain
 {
     public:
-        Terrain( double hauteur, double largeur);
+        Terrain();
+        Terrain(double hauteurTerrain, double largeurTerrain);
+        Terrain(double hauteurTerrain, double largeurTerrain,int nombreColone,int nombreLigne,double hauteurmur,double hauteurRaquette,double largeurRaquette);
         virtual ~Terrain();
-        mure* Tmure() const ;
-        brique* Tbrique() const ;
-        Raquette* TRaquette() const ;
-        balle Tballe() const ;
-        void run(double dt);
-        void initializer_Murs(double hauteur);
-        void initializer_Briques(int nbColone,int nbLigne,double hauteur);
-        void initializer_Raquette(point &position,double hauteur,double largeur);
-        void initializer_Balle();
-        void initializer_Balle(point & position,double vitesse);
         unique_ptr<surface> randomSurface();
-    protected:
+        brique*  randomBrique(point &position,double largeur,double hauteurBrique,int nombreFois=5);
+        Terrain* terrain();
+        vector<mur*> murs() const ;
+        list briques()const;
+        vector<rectangle*> rectangle() const ;
+        Raquette* Raquette() const ;
+        balle* balle() const ;
+        double hauteur() const;
+        double largeur()const;
+        void initializer_Murs(double hauteurmur);
+        void initializer_Briques(int nombreColone,int nombreLigne,double hauteurmur);
+        void initializer_Raquette(double hauteurRaquette,double largeurRaquette,double hauteurmur);
+        void initializer_Balle(double vitesse = 2.5);
+        void afficheRectangle(afficheur* affiche);
+        void affichemurs(afficheur* affiche);
+        void afficheRaquette(afficheur* affiche);
+        void afficheBalle(afficheur* affiche);
+        void RaquetteMoveGauche();
+        void RaquetteMoveDroit();
+        void run(double dt,afficheur* affiche);
+        bool existColusion(cassebrique::balle* b);
+
 
     private:
-        vector<unique_ptr<mure>> d_mure;
-        vector<unique_ptr<brique>> d_brique;
-        unique_ptr<Raquette> d_raquette;
-        balle d_balle;
-        double d_largeur,d_hauteur;
+        vector<unique_ptr<cassebrique::mur>>  d_murs;
+        list  d_briques;
+        unique_ptr<cassebrique::balle> d_balle;
+        unique_ptr<cassebrique::Raquette> d_raquette;
+        double d_largeur;
+        double d_hauteur;
 
 };
 }

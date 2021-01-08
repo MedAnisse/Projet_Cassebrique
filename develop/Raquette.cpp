832fc5343708e414bfe7243384f04e0f8613d235
MedAnisse <Med-anisse.belhadj@outlook.com>
2021-01-08 14:21:05 +0100

    copie raquette de test_raquette ver develop

--- develop/Raquette.cpp
+++ develop/Raquette.cpp
@@ -3,38 +3,83 @@
 #include<iostream>
 #include<ostream>
 #include<istream>
+#include"point.h"
+#include"surface.h"
+#include "vector.h"
+#include "rectangle.h"
+#include"balle.h"
 using std::unique_ptr;
-using std::move;
 using std::make_unique;
+using geom::point;
+class balle;
 namespace cassebrique
 {
-     Raquette::Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK)
-     d_position{position},d_hauteur{hauteur},d_largeur{largeur}
-     {
-         d_surface=move(surfaceK);
-     }
-    Raquette::~Raquette()
-     {
-
-     }
-    surface* Raquette::surfaceType()const
-     {
-        return d_surface.get();
-     }
-    double Raquette::Hauteur()const
-     {
-         return d_hauteur;
-     }
-    double Raquette::Largeur()const
-     {
-        return d_largeur;
-     }
-     void bougerGauche(int n)
-     {
-    // changer la position de raquette vers la gauche avec n pas
-     }
-    void bougerDroit(int n)
+    Raquette::Raquette():rectangle{}
     {
-    // changer la position de raquette vers la droit avec n pas
+    d_surface=make_unique<surface>();
     }
+    Raquette::Raquette(const point& BasGauche,const point& HautDroit):rectangle{BasGauche,HautDroit}
+    {double x{BasGauche.x()},y{HautDroit.y()};
+        point HautGauche{x,y};
+        point HautDroit1=HautDroit;
+        d_surface=make_unique<surface>(HautGauche,HautDroit1);
+    }
+    Raquette::Raquette(point& BasGauche,double hauteur,double largeur):rectangle{BasGauche,hauteur,largeur}
+{double x{BasGauche.x()},y{HautDroit().y()};
+    point HautGauche{x,y};
+
+    point HautDroit1=rectangle::HautDroit();
+    d_surface=make_unique<surface>(HautGauche,HautDroit1);
+}
+    Raquette::~Raquette()
+    {
+
+    }
+    surface* Raquette::surfaceType()const
+    {
+        return d_surface.get();
+    }
+    void Raquette::move_Gauche()
+    {
+        rectangle::d_BasGauche.move(-10.0,0.0);
+        rectangle::d_HautDroit.move(-10.0,0.0);
+        d_surface->move(-10.0,0.0);
+    }
+    void Raquette::move_Droit()
+    {
+        rectangle::d_BasGauche.move(10,0.0);
+        rectangle::d_HautDroit.move(10,0.0);
+        d_surface->move(10.0,0.0);
+    }
+    void Raquette::move_Gauche(int n)
+    {
+        for(int i=0 ;i<n;i++)
+        {
+            move_Gauche();
+        }
+    }
+    void Raquette::move_Droit(int n)
+    {
+        for(int i=0 ;i<n;i++)
+        {
+            move_Droit();
+        }
+    }
+    bool Raquette::OnCollision(balle* balle)const
+{
+    return d_surface->collusion(balle);
+}
+point Raquette::debut()const
+{
+    return d_surface->debut();
+}
+
+point Raquette::fin()const
+{
+    return d_surface->fin();
+}
+double Raquette::distance(balle* balle)
+{
+     return d_surface->distance(balle);
+}
 }
--- develop/Raquette.h
+++ develop/Raquette.h
@@ -1,23 +1,31 @@
 #ifndef RAQUETTE_H
 #define RAQUETTE_H
+#include "rectangle.h"
+#include "surface.h"
+#include<memory>
+using geom::point;
+using std::unique_ptr;
+using std::make_unique;
 namespace cassebrique
 {
-class Raquette
+class Raquette: public rectangle
 {
 public:
-    Raquette(point& position,double hauteur,double largeur,unique_ptr<surface> surfaceK);
+    Raquette();
+    Raquette(const point& BasGauche,const point& HautDroit);
+    Raquette(point& BasGauche,double hauteur,double largeur);
     virtual ~Raquette();
     surface* surfaceType()const;
-    double Hauteur()const;
-    double Largeur()const;
-    void bougerGauche(int n);
-    void bougerDroit(int n);
+    void move_Gauche();
+    void move_Droit();
+    void move_Gauche(int n);
+    void move_Droit(int n);
+    bool OnCollision(balle* balle)const ;
+    point debut()const;
+    point fin()const;
+    double distance(balle* balle);
 private:
     unique_ptr<surface> d_surface;
-    point position;
-    double d_hauteur,d_largeur;
-protected:
-
 };
 }
 #endif // RAQUETTE_H
